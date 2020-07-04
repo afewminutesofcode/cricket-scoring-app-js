eb2cc7a5670d8fdad1e291e0ce090d1d3e3652fd
afewminutesofcode <afewminutesofcode@gmail.com>
2020-07-04 23:36:12 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# cricket-scoring-app
+
+Using ReactJS to build a cricket scoring app.
+Blog post and video tutorial coming soon
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "react-dom";
+
+import "./styles.css";
+const App = () => (
+  <div className="app-wrap">
+    <h1>Cricket Scoing App</h1>
+  </div>
+);
+
+render(<App />, document.getElementById("root"));
